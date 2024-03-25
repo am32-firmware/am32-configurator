c5b6a59d3ecdb5608e7b016a0d496bf645343e36
Eike Ahmels <ea@weslink.de>
2024-03-25 15:04:27 +0100

    styled front page

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,132 @@
   <div>
     <div>
       <TopNavigation />
-      <div class="p-4 max-w-[1400px] m-auto prose prose-invert">
-        <ContentDoc />
+      <div class="p-4 max-w-[1400px] mx-auto pb-16">
+        <div class="flex flex-col gap-4">
+          <UCard>
+            <div class="container">
+              <h1 class="text-3xl font-bold mb-4">
+                AM32 MultiRotor ESC Firmware
+              </h1>
+              <p class="text-lg mb-8">
+                Specialized firmware for speed controllers, designed for
+                multirotor enthusiasts.
+              </p>
+
+              <div class="mb-8">
+                <h2 class="text-2xl font-bold mb-4">
+                  Key Features
+                </h2>
+                <ul class="list-disc list-inside">
+                  <li class="mb-2">
+                    Firmware Upgradeability: Seamless updates via Betaflight passthrough for G4, F4, and F7
+                    flight controllers.
+                  </li>
+                  <li class="mb-2">
+                    Motor Protocol Support: Compatible with Servo PWM and Dshot (300, 600) motor
+                    protocols.
+                  </li>
+                  <li class="mb-2">
+                    Bi-directional Dshot: Enhance control and telemetry with bidirectional communication
+                    support.
+                  </li>
+                  <li class="mb-2">
+                    Telemetry Support: KISS standard ESC telemetry for comprehensive monitoring.
+                  </li>
+                  <li class="mb-2">
+                    Variable PWM Frequency: Customize PWM frequency for precise motor control.
+                  </li>
+                  <li class="mb-2">
+                    Sinusoidal Startup Mode: Smooth acceleration for larger motors.
+                  </li>
+                </ul>
+              </div>
+
+              <div class="mb-8">
+                <h2 class="text-2xl font-bold mb-4">
+                  Firmware Release & Configuration
+                </h2>
+                <p class="mb-4">
+                  Stay updated with the latest firmware releases and configure settings using the
+                  Multi_ESC config tool and Betaflight passthrough. Alternatively, use the Online-ESC Configurator for
+                  web-based flashing and configuration adjustments.
+                </p>
+                <p class="mb-4">
+                  Visit the <a href="#">latest release</a> for firmware updates.
+                </p>
+                <p>Download the Multi_ESC config tool for <a href="#">Windows</a> or <a href="#">Linux</a>.</p>
+              </div>
+
+              <div class="mb-8">
+                <h2 class="text-2xl font-bold mb-4">
+                  Hardware Compatibility
+                </h2>
+                <p class="mb-4">
+                  Refer to the target compatibility list for hardware compatibility information and
+                  installation instructions.
+                </p>
+                <p>View the <a href="#">compatibility list</a> (December 2020).</p>
+              </div>
+
+              <div class="mb-8">
+                <h2 class="text-2xl font-bold mb-4">
+                  Support & Community
+                </h2>
+                <p class="mb-4">
+                  Join the dedicated Slack chat channel for support, etiquette guidelines, and community
+                  interaction.
+                </p>
+                <UButton
+                  to="https://discord.com/invite/h7ddYMmEVV"
+                  target="_blank"
+                  icon="i-mdi-discord"
+                  variant="outline"
+                  color="blue"
+                >
+                  Join Discord
+                </UButton>
+              </div>
+
+              <div class="">
+                <h2 class="text-2xl font-bold mb-4">
+                  Contributors
+                </h2>
+                <p class="">
+                  A big thanks to all contributors, developers, and pilots who have contributed time,
+                  advice, and code to the AM32 project.
+                </p>
+              </div>
+            </div>
+          </UCard>
+          <UCard>
+            <template #header>
+              <div class="text-center text-xl font-bold text-green-500">
+                Support
+              </div>
+            </template>
+            <div class="text-center">
+              Running and maintaining projects like ours requires continuous effort and resources. If you find our work valuable and would like to support us, consider becoming a patron on Patreon or making a one-time contribution via PayPal.
+            </div>
+            <template #footer>
+              <div class="flex justify-center gap-4 text-4xl">
+                <NuxtLink
+                  to="https://www.patreon.com/user?u=44228479"
+                  target="_blank"
+                  external
+                >
+                  <UIcon name="i-mdi-patreon" />
+                </NuxtLink>
+                <NuxtLink
+                  to="https://www.patreon.com/user?u=44228479"
+                  target="_blank"
+                  external
+                >
+                  <UIcon name="i-mdi-paypal" />
+                </NuxtLink>
+              </div>
+            </template>
+          </UCard>
+        </div>
       </div>
     </div>
   </div>
