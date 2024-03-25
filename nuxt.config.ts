be14017a9236cf2b32fa360655e1f44455c7249d
Eike Ahmels <ea@weslink.de>
2024-03-25 12:36:06 +0100

    revert nitro edge functions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    nitro: {
-        preset: 'netlify-edge'
-    },
     devtools: {
         enabled: true,
 
