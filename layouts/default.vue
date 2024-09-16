45a4ea65d06ea6346124fc6927ced2b0fb284de1
Eike Ahmels <eike.ahmels@zeitmechanik.net>
2024-09-16 09:56:34 +0200

    chore

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,7 +26,7 @@
         </div>
         <div class="text-center min-w-[300px] flex items-center gap-4">
           <div>
-            <img src="https://api.netlify.com/api/v1/badges/1735dfe5-3b7a-45c7-932a-0787e682b1b9/deploy-status" alt="netlify status">
+            <img :src="badgeUrl" alt="netlify status">
           </div>
           <div>
             <NuxtLink class="underline" to="https://github.com/am32-firmware/am32" target="_blank" external>
@@ -54,6 +54,10 @@
 </template>
 <script setup lang="ts">
 import logo from '~/assets/icons/am32-logo.svg';
+const location = useBrowserLocation();
+const badgeUrl = computed(() =>
+    `https://api.netlify.com/api/v1/badges/1735dfe5-3b7a-45c7-932a-0787e682b1b9/deploy-status${location.value.href?.includes('develop') ? '?branch=develop' : ''}`
+);
 const serialStore = useSerialStore();
 const logStore = useLogStore();
 </script>
