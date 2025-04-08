23496ba78ee1edf6c3b5a6a5129e9601286f5ce2
Eike Ahmels <eike.ahmels@zeitmechanik.net>
2025-04-08 18:07:13 +0200

    chore

--- src/mcu.ts
+++ src/mcu.ts
@@ -74,7 +74,7 @@ class Mcu {
     static BOOT_LOADER_VERSION_SIZE = 1;
 
     static PORT_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    static PIN_CHARACTERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    static PIN_CHARACTERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
 
     static parseBootLoaderPin (pin: number): [boolean, string] {
         const port = pin >> 4;
