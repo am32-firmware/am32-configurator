46688daff14e903e454488db675584a19496a8c9
Eike Ahmels <eike.ahmels@zeitmechanik.net>
2025-11-27 09:39:54 +0100

    fix schema middleware

--- server/middleware/json.ts
+++ server/middleware/json.ts
@@ -1,8 +1,6 @@
 export default defineEventHandler((event) => {
     const url = getRequestURL(event);
-    console.log(event.node);
     if (url.pathname.endsWith('json')) {
-        // sendRedirect(event, url.pathname.replace(/\.json$/, ''));
-        // event.context.matchedRoute = url.pathname.replace(/\.json$/, '');
+        sendRedirect(event, url.pathname.replace(/\.json$/, ''));
     }
 });
