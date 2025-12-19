37accf472b871b2c0e4f3f38e05b0985c286286f
Eike Ahmels <eike.ahmels@zeitmechanik.net>
2025-12-19 15:33:27 +0100

    wip

--- plugins/matomo.ts
+++ plugins/matomo.ts
@@ -1,11 +1,9 @@
 import VueMatomo from 'vue-matomo';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    if (process.env.NODE_ENV === 'production') {
-        nuxtApp.vueApp.use(VueMatomo, {
-            host: 'https://matomo.am32.ca',
-            router: useRouter(),
-            siteId: 1
-        });
-    }
+    nuxtApp.vueApp.use(VueMatomo, {
+        host: 'https://matomo.am32.ca',
+        router: useRouter(),
+        siteId: 1
+    });
 });
